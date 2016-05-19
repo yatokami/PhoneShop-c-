6760933ade6bd58e8950041f5d4c4d4a0ecefdc0
yatokami <873550063@qq.com>
2016-05-19 09:39:36 +0800

    s2

---
+++ ViewModel/WellBadView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class WellBadView
+    {
+        public string well;
+        public string Well { get { return "赞" + well; } set { well = value; } }
+
+        public string bad;
+        public string Bad { get { return "踩" + bad; } set { bad = value; } }
+    }
+}
